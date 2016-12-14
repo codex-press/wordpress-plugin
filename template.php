73e02de913d7ff8c2a3b6bed79f4c2fa1149cdd8
Tim Farnam <twfarnam@gmail.com>
2016-12-14 17:28:59 -0500

    fixes embed url to be usercontent

--- template.php
+++ template.php
@@ -3,7 +3,7 @@
   get_header();
 
   $url = get_post_meta( get_the_ID() , 'cp_url', true );
-  $url = 'https://codex.press' . $url . '?embed';
+  $url = 'https://usercontent.codex.press' . $url . '?embed';
 
   if ( get_post_meta( get_the_ID() , 'cp_include_menus', true ) )
     $style = 'width:100%;height:100vh;background:white;';
